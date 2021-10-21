33ed6acdc6804863b461f36b7f7d959c0df2fcdc
Makoto Sugahara <30372121+mi-os@users.noreply.github.com>
2021-10-21 23:51:15 +0900

    add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => (
+  <footer>
+    <dic className="grid justify-items-center">
+      <p>This is Footer area!!</p>
+    </dic>
+  </footer>
+);
